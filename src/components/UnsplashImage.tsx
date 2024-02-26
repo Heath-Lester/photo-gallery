138d95224daf9445c88c5793f4dfb827915f5c7f
Heath Lester <h.allen.lester@gmail.com>
2024-02-26 05:36:11 -0500

    fix card

--- src/components/UnsplashImage.tsx
+++ src/components/UnsplashImage.tsx
@@ -19,26 +19,26 @@ export default async function UnsplashImage({ id }: { id: string }): Promise<Rea
         const cardWidth: number = Math.ceil(500 * heightWidthRatio);
 
         return (
-            <Card className='flex flex-row h-full w-full relative overflow-hidden gap-4 px-4 py-3'>
+            <Card className='flex flex-row h-full w-full relative overflow-hidden gap-3 px-4 py-3'>
                 <img
                     alt={image.alt_description ?? 'description'}
                     src={image.urls.full}
-                    className='object-cover h-[85vh] max-w-[85vh] rounded-md'
+                    className='object-cover h-[85vh] max-w-[85vh] rounded-md shadow-inner'
                     sizes='500px'
                 />
-                <section className='flex flex-col p-2 text-small items-center'>
-                    <img
-                        alt='Profile Image'
-                        src={image.user.profile_image.large}
-                        className='rounded-full h-24 w-24 mb-6'
-                    />
-                    <div className='flex flex-col gap-4'>
+                <section>
+                    <Card className='flex flex-col mx-2 text-small font-semibold gap-3 p-4 shadow-md'>
+                        <img
+                            alt='Profile Image'
+                            src={image.user.profile_image.large}
+                            className='rounded-full h-32 w-32 my-2 shadow-inner content-center mx-auto'
+                        />
                         <p>{image.user.name}</p>
                         <p>Instagram: {image.user.instagram_username}</p>
                         <p>Location: {image.user.location}</p>
                         <p>{image.user.bio}</p>
                         <p>{image.description}</p>
-                    </div>
+                    </Card>
                 </section>
             </Card>
         );
