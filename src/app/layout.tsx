b7f7230f867e7674030fdab6ab819c6efed172e3
Heath Lester <h.allen.lester@gmail.com>
2024-02-26 06:32:22 -0500

    figuring out parallel routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Providers from './providers';
 import NavBar from '@/components/NavBar';
 import React from 'react';
+import { ScriptProps } from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,20 +13,16 @@ export const metadata: Metadata = {
     description: 'Coding Assessment for MicroHealth',
 };
 
-export default function RootLayout({
-    modal,
-    children,
-}: {
-    modal: React.ReactNode;
-    children: React.ReactNode;
-}): React.ReactNode {
+export default function RootLayout(props: { children: React.ReactNode; parallel: React.ReactNode }): React.ReactNode {
     return (
         <html lang='en'>
             <body className={inter.className}>
                 <Providers>
                     <NavBar />
-                    <div>{modal}</div>
-                    <main>{children}</main>
+                    <main>
+                        {props.children}
+                        {props.parallel}
+                    </main>
                 </Providers>
             </body>
         </html>
--- src/components/UnsplashModal.tsx
+++ src/components/UnsplashModal.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import { Link } from '@nextui-org/react';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
-export default function UnsplashModal({ children }: { children: React.ReactNode }): React.ReactNode {
-    console.warn('UNSPLASH MODAL ACTIVATED: ', !!children);
+export default function UnsplashModal({ children }: { children: React.ReactNode }): React.ReactNode | null {
+    const pathname: string = usePathname();
+
+    if (pathname === '/') return null;
+
     return (
         <dialog className='flex shadow-lg fixed justify-self-center self-center z-40 rounded-lg backdrop-blur'>
             <Link href='/' />
