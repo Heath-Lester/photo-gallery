bf577c3f1cdeb9bd6d43ab8dd9b387e132895f75
Heath Lester <h.allen.lester@gmail.com>
2024-02-27 21:54:49 -0500

    remove metadata function

--- src/app/keyword/[term]/[id]/page.tsx
+++ src/app/keyword/[term]/[id]/page.tsx
@@ -1,12 +1,7 @@
 import ModalPlaceholder from '@/components/ModalPlaceholder';
 import UnsplashImage from '@/components/UnsplashImage';
 import { ModalParameters } from '@/types/modalParameters';
-import { Metadata } from 'next';
 import React, { Suspense } from 'react';
-
-export function generateMetadata({ params: { term, id } }: ModalParameters): Metadata {
-    return { title: `Photo: ` + id };
-}
 
 export default function PhotoPage({ params: { term, id } }: ModalParameters): React.ReactNode | void {
     return (
