f6b6023576462e0367ceecf5700930f2aba6e5ec
Heath Lester <h.allen.lester@gmail.com>
2024-02-27 21:51:15 -0500

    change default to tsx

--- src/components/UnsplashClientModal.tsx
+++ src/components/UnsplashClientModal.tsx
@@ -22,8 +22,11 @@ export default function UnsplashClientModal({ children }: { children: React.Reac
         );
     } else {
         return (
-            <div onClick={handleClose} className='flex w-full h-full fixed top-0 left-0 z-30 backdrop-blur-sm'>
-                <dialog className='flex z-50 rounded-2xl justify-self-center self-center'>
+            <div
+                onClick={handleClose}
+                className='flex w-full h-full overflow-auto fixed top-0 left-0 z-30 backdrop-blur-sm'
+            >
+                <dialog className='flex z-50 rounded-2xl justify-self-center self-center my-8'>
                     <Suspense fallback={<ModalPlaceholder />}>{children}</Suspense>
                 </dialog>
             </div>
--- src/components/UnsplashImage.tsx
+++ src/components/UnsplashImage.tsx
@@ -19,21 +19,13 @@ export default async function UnsplashImage({ id }: { id: string }): Promise<Rea
         const heightWidthRatio: number = image.width / image.height;
 
         return (
-            <div
-                // radius='lg'
-                // className='flex flex-row relative overflow-hidden gap-4 p-4 justify-center items-center align-middle'
-                className='flex flex-col-reverse md:flex-row gap-4 p-4 rounded-lg shadow-2xl'
-            >
+            <div className='flex flex-col-reverse lg:flex-row gap-4 p-4 rounded-lg shadow-2xl'>
                 <img
                     alt={image.alt_description ?? 'description'}
                     src={image.urls.full}
                     className={`rounded-2xl shadow-inner max-h-[80vh] max-w[80vw] h-[calc(80vh * ${heightWidthRatio}) - 192px] w-[calc(80vw * ${widthHeightRatio}) - 192px]`}
-                    // className='max-h-[80vh] h-[80vh * ${heightWidthRatio}] max-w-[80vw] w-[80vw * ${widthHeightRatio}] rounded-lg shadow-inner'
-                    // className={`object-cover max-h-[80vh] h-[80vh * ${heightWidthRatio}] max-w-[80vw] w-[80vw * ${widthHeightRatio}] rounded-md shadow-inner`}
-                    // className={`object-cover max-h-[80vh] h-[80vh * ${heightWidthRatio}] max-w-[80vw] w-[80vw * ${widthHeightRatio}] rounded-md shadow-inner`}
-                    // sizes='500px'
                 />
-                <section className='flex flex-row-reverse justify-end md:flex-col max-h-42 md:max-h-[unset] md:mb-auto text-small font-semibold gap-4 p-4 md:max-w-48 md:min-w-48 shadow-md rounded-lg'>
+                <section className='flex flex-row-reverse justify-between lg:flex-col max-h-42 lg:max-h-[unset] lg:mb-auto text-small font-semibold gap-4 p-4 lg:max-w-48 lg:min-w-48 shadow-md rounded-lg'>
                     <Image
                         alt='Profile Image'
                         src={image.user.profile_image.large}
@@ -42,7 +34,7 @@ export default async function UnsplashImage({ id }: { id: string }): Promise<Rea
                         sizes='144px'
                         className='rounded-full self-center'
                     />
-                    <div className='flex flex-col sm:mr-auto overflow-y-auto shadow-inner'>
+                    <div className='flex flex-col overflow-y-auto shadow-inner'>
                         <UnsplashImageDetail title='Name' content={image.user.name} />
                         <UnsplashImageDetail title='Instagram' content={image.user.instagram_username} />
                         <UnsplashImageDetail title='Location' content={image.user.location} />
