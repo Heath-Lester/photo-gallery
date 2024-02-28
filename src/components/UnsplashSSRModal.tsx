1c129d2e0a457f6deb609181c63ba143e89acf2c
Heath Lester <h.allen.lester@gmail.com>
2024-02-27 22:48:17 -0500

    match modal stylings

--- src/components/UnsplashSSRModal.tsx
+++ src/components/UnsplashSSRModal.tsx
@@ -12,9 +12,9 @@ export default function UnsplashSSRModal({
     return (
         <Link
             href={returnPath ?? '/'}
-            className='flex justify-center items-center content-center w-full h-full fixed top-0 left-0 z-30 backdrop-blur-sm'
+            className='flex w-full h-full overflow-auto fixed top-0 left-0 z-30 backdrop-blur-sm'
         >
-            <dialog className='flex z-50 rounded-xl'>
+            <dialog className='flex z-50 rounded-2xl justify-self-center self-center my-8'>
                 <Suspense fallback={<ModalPlaceholder />}>{children}</Suspense>
             </dialog>
         </Link>
