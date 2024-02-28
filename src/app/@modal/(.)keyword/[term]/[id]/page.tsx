112fa78c5ab01885ce5daea48c4a2df01ef17f18
Heath Lester <h.allen.lester@gmail.com>
2024-02-27 23:44:06 -0500

    add metadata functions

--- src/app/@modal/(.)keyword/[term]/[id]/page.tsx
+++ src/app/@modal/(.)keyword/[term]/[id]/page.tsx
@@ -4,6 +4,11 @@ import React from 'react';
 import UnsplashClientModal from '@/components/UnsplashClientModal';
 import UnsplashSSRModal from '@/components/UnsplashSSRModal';
 import { UnsplashSearchTypes } from '@/enums/unsplashSearchTypes';
+import { Metadata } from 'next';
+
+export function generateMetadata({ params: { term, id } }: ModalParameters): Metadata {
+    return { title: `Photo Gallery: ` + term };
+}
 
 export default function InterceptingPhotoPage({ params: { term, id } }: ModalParameters): React.ReactNode {
     const ssrModal: React.ReactNode = (
--- src/app/keyword/[term]/[id]/page.tsx
+++ src/app/keyword/[term]/[id]/page.tsx
@@ -1,7 +1,12 @@
 import ModalPlaceholder from '@/components/ModalPlaceholder';
 import UnsplashImage from '@/components/UnsplashImage';
 import { ModalParameters } from '@/types/modalParameters';
+import { Metadata } from 'next';
 import React, { Suspense } from 'react';
+
+export function generateMetadata({ params: { term, id } }: ModalParameters): Metadata {
+    return { title: `Photo Gallery: ` + term };
+}
 
 export default function PhotoPage({ params: { term, id } }: ModalParameters): React.ReactNode | void {
     return (
